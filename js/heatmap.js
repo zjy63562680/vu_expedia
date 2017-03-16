8fba3fad68ea3b01f93aa13aa33ee979e119b1b5
Jessicavanharten <jessicavanharten@hotmail.com>
2017-03-16 22:28:17 +0100

    Arranging data

--- js/heatmap.js
+++ js/heatmap.js
@@ -1,10 +1,14 @@
+// Load the data
 d3.queue()
 
+    // Point to the data files
     .defer(d3.csv, "data/success_europe_filter.csv")
     .defer(d3.csv, "data/failed_europe_filter.csv")
+
+    // When the data is loaded, run the function 'onDataLoaded'
     .await(onDataLoaded);
 
-
+// Check the data is properly loadeed
 function onDataLoaded(error, dataSucess, dataFailed) {
 
     if (error) {
@@ -16,11 +20,56 @@ function onDataLoaded(error, dataSucess, dataFailed) {
 
 };
 
+// Filter the data
 function filterData(data) {
 
     var filteredData = d3.nest()
-        .key(function(d) { return d.currency; })
+
+        // Arrange data into months
+        .key(function(d) {
+            var timestampRaw = d.launched_at * 1000, // Data needs '000' appended to be a valid timestamp
+                timestampDate = new Date(timestampRaw), // Converts integer into date object
+                timestampMonth = timestampDate.getMonth(); // Gets the month from the date object
+            return timestampMonth;
+        }).sortKeys((a, b) => d3.ascending(+a, +b)) // Sort by numerical value
+
+        // Then arrange data into top level category
+        .key(function(d) {
+            return d["category/parent_id"];
+        }).sortKeys((a, b) => d3.ascending(+a, +b)) // Sort by numerical value
+
+        // Then arrange data into sub category
+        .key(function(d) {
+            return d["category/id"];
+        }).sortKeys((a, b) => d3.ascending(+a, +b)) // Sort by numerical value
+
         .entries(data);
+
     console.log(filteredData);
 
 }
+
+var dataStructure = {
+    "monthName": {
+        "total": 0,
+        "failed": 0,
+        "success": 0,
+        "percentile": 100,
+        "categories": {
+            "categoryName": {
+                "all": {
+                    "total": 0,
+                    "failed": 0,
+                    "success": 0,
+                    "percentile": 100,
+                },
+                "subCategoryName": {
+                    "total": 0,
+                    "failed": 0,
+                    "success": 0,
+                    "percentile": 100,
+                },
+            }
+        }
+    },
+};
