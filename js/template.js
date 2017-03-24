3d2f9e71e5cad7f7fef0ad2159b195a6de2e460a
Amaka Atoyebi <amaka.atoyebi@gmail.com>
2017-03-24 09:07:06 -0400

    Edits to template file to incorporate page scraping

--- js/template.js
+++ js/template.js
@@ -2,6 +2,8 @@
 document.addEventListener("DOMContentLoaded", function() {
 
     bindNavigation();
+    handleUrlRequest();
+    hideShowViz('hide');
     // URL entered event?
 
 });
@@ -29,3 +31,57 @@ function bindNavigation() {
 
     });
 }
+
+function handleUrlRequest () {
+    $('.submit').on('click', function (e) {
+        e.preventDefault();
+        var input = $('.url-input');
+        var def = $.Deferred();
+        var self = this;
+        self.json = { category : '', country : '', goal: ''};
+
+        $.ajax({
+            type: "GET",
+            url: $(input).val(), 
+            data: input.serialize(), // serializes the form's elements.
+            success: function(data) {
+                var title, country, category;
+
+                $(data).find('a[href*="places"]').filter(function(){
+                  var data = $(this);
+                  country = data.text().trim().split(", ").pop();
+
+                  self.json.country = country;
+                });
+
+                $(data).find('a[href*="categories"]').filter(function(){
+                  var data = $(this);
+                  category = data.text().trim();
+
+                  self.json.category = category;
+                });
+
+                $(data).find('div.num').filter(function(){
+                  var data = $(this);
+                  goal = data.attr('data-goal');
+
+                  self.json.goal = goal;
+                });
+
+                def.resolve();
+                return def.promise;
+            }
+        }).then(function () {
+            hideShowViz('show');
+        });
+    });
+}
+
+function hideShowViz (hideShow) {
+    if (hideShow === 'hide') {
+        $('.bubble_chart').hide();
+    } else if (hideShow === 'show') {
+        $('.bubble_chart').show();
+    } 
+}
+
