efba087592841f1137986e8637afcbb7c08dd978
Amaka Atoyebi <amaka.atoyebi@gmail.com>
2017-03-26 19:33:56 -0400

    Finalized pulling of trends data

--- server.js
+++ server.js
@@ -7,40 +7,38 @@ var http = require('http');
 var path = require("path");
 var util = require('util');
 var bodyParser = require("body-parser");
+var list = [];
 var count = 0;
+
 function processUrl (req, res, keyword) {
-    function repeat (arr) {
-      console.log(arr, arr.length);
-      googleTrends.interestOverTime({keyword: keyword, startTime: new Date('2016-02-01'), endTime: new Date('2017-02-01')})
+  function repeat (item) {
+    console.log('first' + list.length, count, item, item[count]);
+    if (!item[count]) {
+      count = 0;
+      return;
+    } else {
+      googleTrends.interestOverTime({keyword: item[count], startTime: new Date('2016-02-01'), endTime: new Date('2017-02-01')})
         .then(function(results){
-          fs.writeFile('google_related_ranks.json', JSON.stringify(results, null, 4), function(err){
+          list.push(results);
+          count++;
+          fs.writeFile('google_related_ranks.json', JSON.stringify(list, null, 4), function(err){
             console.log('File successfully written! - Check your project directory for the google.json file');
           });
+          repeat(item);
         })
         .catch(function(err){
           console.error('Oh no there was an error in more', err);
       });
-    } 
-  if (keyword && keyword.length) {
-    if (keyword.length > 2) {
-      /*var i,j,temparray,chunk = 2;
-      for (i=0,j=keyword.length; i<j; i+=chunk) {
-          temparray = keyword.slice(i,i+chunk);
-          count++;*/
-          repeat(keyword);
-      //}
-    } else if (keyword && keyword.length < 2) {
-      googleTrends.interestOverTime({ keyword: keyword, startTime: new Date('2016-02-01'), endTime: new Date('2017-02-01')})
-        .then(function(results){
-          fs.writeFile('google_main_ranks.json', JSON.stringify(results, null, 4), function(err){
-            console.log('Main file successfully written! - Check your project directory for the google.json file');
-          });
-        })
-        .catch(function(err){
-          console.error('Oh no there was an error in less 1', err);
-      });
+    }
+  } 
 
-      googleTrends.relatedTopics({keyword: keyword, startTime: new Date('2016-02-01'), endTime: new Date('2017-02-01')})
+  if (keyword && keyword.length) {
+    if (keyword.length > 1) {
+       repeat (keyword);
+    } else if (keyword.length === 1) {
+      repeat (keyword);
+
+      googleTrends.relatedTopics({keyword: keyword[0], startTime: new Date('2016-02-01'), endTime: new Date('2017-02-01')})
         .then(function (results) {
           fs.writeFile('google_related.json', JSON.stringify(results, null, 4), function(err){
             console.log('Related file successfully written! - Check your project directory for the google.json file');
@@ -50,7 +48,7 @@ function processUrl (req, res, keyword) {
           console.log('Oh no there was an error in less 2', err);
       });
     } else {
-      console.log('Yooo');
+      console.log('Yooo');  
     }
   }  else {
     console.log('Good luck');
