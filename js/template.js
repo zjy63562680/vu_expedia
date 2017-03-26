b18092ee782086b188ea203848a1fbb55d08680b
Amaka Atoyebi <amaka.atoyebi@gmail.com>
2017-03-26 16:56:24 -0400

    Taking related topics and outputting their own JSON files with rank over time

--- js/template.js
+++ js/template.js
@@ -34,13 +34,13 @@ function bindNavigation() {
 }
 
 function handleUrlRequest () {
-$('.submit').on('click', function (e) {
+    $('.submit').on('click', function (e) {
         e.preventDefault();
         var input = $('.url-input');
         json = {};
         $.ajax({
             type: "GET",
-            url: $(input).val(),
+            url: $(input).val(), 
             data: input.serialize(), // serializes the form's elements.
         }).then(function (data) {
             var title, country, category, sub_category, goal;
@@ -52,11 +52,11 @@ $('.submit').on('click', function (e) {
                   json.country = country;
                 });
 
-                $(data).find('a[href*="ref=category"]:not(.btn--green)').filter(function(){
+                 $(data).find('a[href*="ref=category"]:not(.btn--green)').filter(function(){
                   var data = $(this);
                   category = data[0].href.split('/categories/')[1];
                   category = category.split('/', 1)[0];
-                  console.log(category);
+
                   sub_category = data.text().trim();
 
                   json.category = category.toLowerCase();
@@ -82,11 +82,24 @@ $('.submit').on('click', function (e) {
                 });
                 window.dispatchEvent(event);
                 $('.cust-viz.viz-2').html('<iframe class="bubble_chart" src="kickstarter_viz/bubble_chart.html" height="500"></iframe>');
+                $('.cust-viz.viz-4').html('<iframe class="bubble_chart" src="kickstarter_viz/trend_graph.html" height="500"></iframe>');
                 hideShowViz(1);
             }
+        }).then(function () {
+            $.ajax({
+                url: '/',
+                // dataType: "jsonp",
+                data: JSON.stringify([json.category]),
+                jsonpCallback: 'callback', // this is not relevant to the POST anymore
+                success: function (data) {
+                    console.log('Success: ');
+                },
+                error: function (xhr, status, error) {
+                    console.log('Error: ' + error.message);
+                },
+            });
         });
     });
-
 }
 
 function hideShowViz (hideShow) {
@@ -94,9 +107,7 @@ function hideShowViz (hideShow) {
         $('.bubble_chart').hide();
     } else if (hideShow === 1) {
         $('.bubble_chart').show();
-    }
+    } 
 }
-
-//}
 
 // Fire functions when the page has loaded
--- server.js
+++ server.js
@@ -1,45 +1,62 @@
 var express = require('express');
 var fs = require('fs');
 var request = require('request');
-var cheerio = require('cheerio');
 var googleTrends = require('google-trends-api');
 var app = express();
 var http = require('http');
 var path = require("path");
-var countrynames = require("countrynames");
 var util = require('util');
+var bodyParser = require("body-parser");
 
 function processUrl (req, res, keyword) {
-  googleTrends.interestOverTime({keyword: keyword, startTime: new Date('2016-02-01'), endTime: new Date('2017-02-01')})
-    .then(function(results){
-      //console.log('These results are awesome', results);
-      fs.writeFile('google.json', JSON.stringify(results, null, 4), function(err){
-        //console.log('File successfully written! - Check your project directory for the google.json file');
+  if (keyword && keyword.length > 1) {
+      googleTrends.interestOverTime({keyword: keyword, startTime: new Date('2016-02-01'), endTime: new Date('2017-02-01')})
+        .then(function(results){
+          fs.writeFile('google_related_ranks' + i + '.json', JSON.stringify(results, null, 4), function(err){
+            count++;//console.log('File successfully written! - Check your project directory for the google.json file');
+          });
+        })
+        .catch(function(err){
+          console.error('Oh no there was an error in more', err);
       });
-    })
-    .catch(function(err){
-      console.error('Oh no there was an error', err);
-  });
+  } else if (keyword && keyword.length === 1) {
+    googleTrends.interestOverTime({ keyword: keyword[0], startTime: new Date('2016-02-01'), endTime: new Date('2017-02-01')})
+        .then(function(results){
+          fs.writeFile('google_main_ranks.json', JSON.stringify(results, null, 4), function(err){
+            //console.log('File successfully written! - Check your project directory for the google.json file');
+          });
+        })
+        .catch(function(err){
+          console.error('Oh no there was an error in less 1', err);
+      });
 
-  googleTrends.relatedTopics({keyword: keyword, startTime: new Date('2016-02-01'), endTime: new Date('2017-02-01')})
-    .then(function (results) {
-          
-    })
-    .catch(function (err) {
-      console.log(err);
-  });
-
+      googleTrends.relatedTopics({keyword: keyword[0], startTime: new Date('2016-02-01'), endTime: new Date('2017-02-01')})
+        .then(function (results) {
+          fs.writeFile('google_related.json', JSON.stringify(results, null, 4), function(err){
+            //console.log('File successfully written! - Check your project directory for the google.json file');
+          });
+        })
+        .catch(function (err) {
+          console.log('Oh no there was an error in less 2', err);
+      });
+  } else {
+    console.log('Good luck');
+  }
+  
   res.sendFile(path.join(__dirname + '/index.html'));
-  req.on('data', function (chunk) {
-      console.log('GOT DATA!');
-  });
 }
 
 app.get('/', function(req, res){
-  util.log(util.inspect(req)); // this line helps you inspect the request so you can see whether the data is in the url (GET) or the req body (POST)
-    util.log('Request recieved: \nmethod: ' + req.method + '\nurl: ' + req.url); // this line logs just the method and url
+  //util.log(util.inspect(req)); // this line helps you inspect the request so you can see whether the data is in the url (GET) or the req body (POST)
+  //util.log('Request recieved: \nmethod: ' + req.method + '\nurl: ' + req.url); // this line logs just the method and url
   url = 'https://www.kickstarter.com/projects/1449277917/1681752382?token=b9cbe6aa';
-  processUrl(req, res, req.url.split('?')[1]);
+  var title = unescape(req.url);
+  var parsed;
+
+    if (title && title.indexOf('?') > -1) {
+      parsed = JSON.parse(title.split('?')[1]);
+    }
+    processUrl(req, res, parsed);
 });
 
 app.post('/', function(req, res){
@@ -47,12 +64,14 @@ app.post('/', function(req, res){
 
 app.use(express.static(__dirname + '/'));
 
+app.use(bodyParser.json({}));
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
-app.listen('8888');
+app.listen('8090');
 
 exports = module.exports = app;
