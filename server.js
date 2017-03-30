493f5802a6cdd2c4e48197cbb31e7e34c0014cc2
Amaka Atoyebi <amaka.atoyebi@gmail.com>
2017-03-30 04:07:49 -0400

    Server

--- server.js
+++ server.js
@@ -23,9 +23,9 @@ app.get('/', function(req, res){
       parsedCategory = parsed[0];
       parsedName = parsed[1];
       google(parsedCategory);
-      googleOvertime(['appel','google']);
-      console.log(parsedName, parsedCategory);
+      googleOvertime(parsedName);
     }
+    console.log(parsedCategory, parsedName);
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
@@ -47,9 +47,9 @@ googleTrends.relatedTopics({keyword: topic, geo: element2})
 var json_file = JSON.parse(res);
 var oj = json_file['default']['rankedList'][0]['rankedKeyword']
 
-for (var x=0; x< oj.length;x++){
-testing.push([geo,oj[x].topic.title, oj[x].value])};
-jsonfile.writeFile('./data/words.txt',testing)
+for(var z=0; z< oj.length;z++){
+testing.push([geo,oj[z].topic.title, oj[z].value])};
+jsonfile.writeFile('./data/wordssss.txt',testing)
 }) }  })}
 
 
@@ -58,43 +58,37 @@ var list = [];
 googleTrends.interestByRegion({keyword: words})
 .then((res) => {
   
-  var json_file = JSON.parse(res);
-  json_file['default']['geoMapData'].forEach(function(element){
+  var json_file1 = JSON.parse(res);
+  json_file1['default']['geoMapData'].forEach(function(element){
   list.push([element['geoCode'],element['formattedValue'][0]])
   })
-  jsonfile.writeFile('./data/trends.txt', list)
+  jsonfile.writeFile('./data/trendssss.txt', list)
 })
 .catch((err) => {
   console.log(err);
 })
 }
-
-
-
 function googleOvertime(word){
-  first='name 2004-01 2004-02 2004-03 2004-04 2004-05 2004-06 2004-07 2004-08 2004-09 2004-10 2004-11 2004-12 2005-01 2005-02 2005-03 2005-04 2005-05 2005-06 2005-07 2005-08 2005-09 2005-10 2005-11 2005-12 2006-01 2006-02 2006-03 2006-04 2006-05 2006-06 2006-07 2006-08 2006-09 2006-10 2006-11 2006-12 2007-01 2007-02 2007-03 2007-04 2007-05 2007-06 2007-07 2007-08 2007-09 2007-10 2007-11 2007-12 2008-01 2008-02 2008-03 2008-04 2008-05 2008-06 2008-07 2008-08 2008-09 2008-10 2008-11 2008-12 2009-01 2009-02 2009-03 2009-04 2009-05 2009-06 2009-07 2009-08 2009-09 2009-10 2009-11 2009-12 2010-01 2010-02 2010-03 2010-04 2010-05 2010-06 2010-07 2010-08 2010-09 2010-10 2010-11 2010-12 2011-01 2011-02 2011-03 2011-04 2011-05 2011-06 2011-07 2011-08 2011-09 2011-10 2011-11 2011-12 2012-01 2012-02 2012-03 2012-04 2012-05 2012-06 2012-07 2012-08 2012-09 2012-10 2012-11 2012-12 2013-01 2013-02 2013-03 2013-04 2013-05 2013-06 2013-07 2013-08 2013-09 2013-10 2013-11 2013-12 2014-01 2014-02 2014-03 2014-04 2014-05 2014-06 2014-07 2014-08 2014-09 2014-10 2014-11 2014-12 2015-01 2015-02 2015-03 2015-04 2015-05 2015-06 2015-07 2015-08 2015-09 2015-10 2015-11 2015-12 2016-01 2016-02 2016-03 2016-04 2016-05 2016-06 2016-07 2016-08 2016-09 2016-10 2016-11 2016-12 2017-01 2017-02 2017-03'+'\n'
-  fs.writeFileSync("./data/test.txt",first);
+  first="name\t2004-01\t2004-02\t2004-03\t2004-04\t2004-05\t2004-06\t2004-07\t2004-08\t2004-09\t2004-10\t2004-11\t2004-12\t2005-01\t2005-02\t2005-03\t2005-04\t2005-05\t2005-06\t2005-07\t2005-08\t2005-09\t2005-10\t2005-11\t2005-12\t2006-01\t2006-02\t2006-03\t2006-04\t2006-05\t2006-06\t2006-07\t2006-08\t2006-09\t2006-10\t2006-11\t2006-12\t2007-01\t2007-02\t2007-03\t2007-04\t2007-05\t2007-06\t2007-07\t2007-08\t2007-09\t2007-10\t2007-11\t2007-12\t2008-01\t2008-02\t2008-03\t2008-04\t2008-05\t2008-06\t2008-07\t2008-08\t2008-09\t2008-10\t2008-11\t2008-12\t2009-01\t2009-02\t2009-03\t2009-04\t2009-05\t2009-06\t2009-07\t2009-08\t2009-09\t2009-10\t2009-11\t2009-12\t2010-01\t2010-02\t2010-03\t2010-04\t2010-05\t2010-06\t2010-07\t2010-08\t2010-09\t2010-10\t2010-11\t2010-12\t2011-01\t2011-02\t2011-03\t2011-04\t2011-05\t2011-06\t2011-07\t2011-08\t2011-09\t2011-10\t2011-11\t2011-12\t2012-01\t2012-02\t2012-03\t2012-04\t2012-05\t2012-06\t2012-07\t2012-08\t2012-09\t2012-10\t2012-11\t2012-12\t2013-01\t2013-02\t2013-03\t2013-04\t2013-05\t2013-06\t2013-07\t2013-08\t2013-09\t2013-10\t2013-11\t2013-12\t2014-01\t2014-02\t2014-03\t2014-04\t2014-05\t2014-06\t2014-07\t2014-08\t2014-09\t2014-10\t2014-11\t2014-12\t2015-01\t2015-02\t2015-03\t2015-04\t2015-05\t2015-06\t2015-07\t2015-08\t2015-09\t2015-10\t2015-11\t2015-12\t2016-01\t2016-02\t2016-03\t2016-04\t2016-05\t2016-06\t2016-07\t2016-08\t2016-09\t2016-10\t2016-11\t2016-12\t2017-01\t2017-02\t2017-03" + '\n'
+  fs.writeFileSync("./data/file1.txt",first);
 
   var index =-1;
   for(x=0 ; x < word.length; x++){
-    googleTrends.interestOverTime({keyword: word[x]}).then((res) => {
+    console.log(word[x])
+    googleTrends.interestOverTime({keyword: word[x]}).then((elet) => {
+      // console.log(elet)
       values = [];
-      json_file = JSON.parse(res);
-      data = json_file['default']['timelineData']
+      json_file = JSON.parse(elet);
+       console.log(json_file)
+      data2 = json_file['default']['timelineData']
       index +=1
-      data.forEach(function(e){
-        var dateString2 = moment.unix(e.time).format("YYYY-MM");
+      data2.forEach(function(e){
         values.push( e.value[0]);
       })
-      if(x ==0){
-        a = values.join("\t")
-        b = word[index]+'\t'+ a+'\n'
-        fs.appendFileSync("./data/test.txt",b)
-      }
-      if(x> 0){
-        c = word[index] + '\t'+ values.join("\t")+'\n'
-        fs.appendFileSync("./data/test.txt",c) 
-      }
+
+        b = word[index]+'\t'+ values.join("\t")+'\n'
+        fs.appendFileSync("./data/file1.txt",b)
+
     })
   }
 }
@@ -113,6 +107,6 @@ app.use(function(req, res, next) {
     next();
 });
 
-app.listen('8050');
+app.listen('8080');
 
 exports = module.exports = app;
